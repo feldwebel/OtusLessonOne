31e4f22c38ff7dc08482b18ba7cca8277a8944b6
feldwebel <daniel@tereschenko.info>
2017-04-13 18:55:43 +0300

    L102 Result

--- L102/src/main/java/application/App.java
+++ L102/src/main/java/application/App.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class App {
 
@@ -10,9 +12,16 @@ public class App {
     public static void main(String[] args) {
         App app = new App();
 
-        String[] dataSet = {"App"};
+        Map<String, Objectivator> dataSet = new HashMap<String, Objectivator>();
+        dataSet.put("App", () -> new App()); //23
+        dataSet.put("String",() -> new String("")); //28
+        dataSet.put("ArrayList", () -> new ArrayList());
+        dataSet.put("int[3]", () -> new int[3]); //36
+        dataSet.put("int[5]", () -> new int[5]); //44
+        dataSet.put("Date", () -> new Date()); //28
 
-        for (String item : dataSet) {
+
+        for (Map.Entry<String, Objectivator> item : dataSet.entrySet()) {
             try {
                 app.checkIt(item);
             } catch (Exception ex) {
@@ -22,38 +31,44 @@ public class App {
 
     }
 
-    private void checkIt(String item) throws InterruptedException, IllegalAccessException, InstantiationException {
+    private void checkIt(Map.Entry<String, Objectivator> item) throws InterruptedException, IllegalAccessException, InstantiationException {
         long before;
         long after;
 
         int size = 1024 * 1024;
-        System.out.println(item);
-        runGC();
-        before = this.sizeOf();
-
+        System.out.println(item.getKey());
         Object[] array = new Object[size];
-        System.out.println("Array of size: " + array.length + " created");
 
         for (int i = 0; i < size; i++) {
-            array[i] = item.getClass().newInstance();
+            array[i] = item.getValue().getNewObject();
         }
+        //runGC();
+        before = this.sizeOf();
+
+        array = null;
+
         runGC();
 
         after = this.sizeOf();
 
-        System.out.println(before + " " + after);
-        System.out.println("Memory spent: " + (after - before));
-        System.out.println("divided " + ((after - before) / size));
+        System.out.println("Memory spent: " + (before - after));
+        System.out.println("divided " + ((before - after) / size));
+
     }
 
     private void runGC() throws InterruptedException {
         runtime.gc();
-        Thread.sleep(1000);
+        Thread.sleep(5000);
         runtime.gc();
     }
 
     private long sizeOf()
     {
         return runtime.totalMemory () - runtime.freeMemory ();
+    }
+
+    @FunctionalInterface
+    private interface Objectivator{
+        Object getNewObject();
     }
 }
