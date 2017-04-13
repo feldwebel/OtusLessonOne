11344b29f48bf3a6d41cadcffa613ed5ce04406e
feldwebel <daniel@tereschenko.info>
2017-04-13 17:47:03 +0300

    L102 draft

--- L102/src/main/java/application/App.java
+++ L102/src/main/java/application/App.java
@@ -1,40 +1,59 @@
 package application;
 
-import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
-
-import java.lang.management.ManagementFactory;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class App {
 
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Starting pid: " + ManagementFactory.getRuntimeMXBean().getName());
-        //Runtime runtime = Runtime.getRuntime();
-        //RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+    protected Runtime runtime = Runtime.getRuntime();
 
-        int size = 5 * 1024 * 1024;
-        Object[] array = new Object[size];
-        System.out.println("Array of size: " + array.length + " created");
-        //Thread.sleep(10 * 1000);
+    public static void main(String[] args) {
+        App app = new App();
 
-        int n = 0;
-        System.out.println("Starting the loop");
-        while (n < Integer.MAX_VALUE) {
-            int i = n % size;
-            array[i] = new String("zhopa"); //no String pool
-            n++;
-            if (n % size == 0) {
-                long oSize = ObjectSizeCalculator.getObjectSize(array);
-                System.out.println("Created " + n + " objects");
-                System.out.println(oSize / size);
-                //System.out.println(ObjectSizeCalculator.getObjectSize(new gnu.trove.map.hash.TObjectIntHashMap<String>(12000, 0.6f, -1)));
-                System.out.println(ObjectSizeCalculator.getObjectSize(new HashMap<String, Integer>(100000)));
-                System.out.println(ObjectSizeCalculator.getObjectSize(3));
-                System.out.println(ObjectSizeCalculator.getObjectSize(new int[]{1, 2, 3, 4, 5, 6, 7 }));
-                System.out.println(ObjectSizeCalculator.getObjectSize(new int[100]));
+        String[] dataSet = {"App"};
 
-                System.out.println(Instrument.sizeOf(new int[100]));
+        for (String item : dataSet) {
+            try {
+                app.checkIt(item);
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
             }
         }
+
+    }
+
+    private void checkIt(String item) throws InterruptedException, IllegalAccessException, InstantiationException {
+        long before;
+        long after;
+
+        int size = 1024 * 1024;
+        System.out.println(item);
+        runGC();
+        before = this.sizeOf();
+
+        Object[] array = new Object[size];
+        System.out.println("Array of size: " + array.length + " created");
+
+        for (int i = 0; i < size; i++) {
+            array[i] = item.getClass().newInstance();
+        }
+        runGC();
+
+        after = this.sizeOf();
+
+        System.out.println(before + " " + after);
+        System.out.println("Memory spent: " + (after - before));
+        System.out.println("divided " + ((after - before) / size));
+    }
+
+    private void runGC() throws InterruptedException {
+        runtime.gc();
+        Thread.sleep(1000);
+        runtime.gc();
+    }
+
+    private long sizeOf()
+    {
+        return runtime.totalMemory () - runtime.freeMemory ();
     }
 }
